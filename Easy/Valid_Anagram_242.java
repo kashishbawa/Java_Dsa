56b46dd3940a29c5c496f41b4911d76eea3e44b9
kashish kumar <kashishkumar8566@gmail.com>
2026-07-04 22:38:09 +0530

    Convert Easy from submodule to normal folder

---
+++ Easy/Valid_Anagram_242.java
@@ -0,0 +1,24 @@
+class Valid_Anagram_242{
+
+     static int fib(int n){
+           
+        if(n == 0 ){
+            return 0;
+        }
+        else if(n ==1){
+            return 1;
+        } 
+        else{
+
+            return  fib(n-1)+fib(n-2);
+            
+        }
+            }
+           
+     
+
+    public static void main(String[] args){
+          
+        System.out.println(fib(8));
+    }
+}
